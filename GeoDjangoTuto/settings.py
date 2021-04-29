b564a0d346651a18cf813700d585c91b3c28b691
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-04-29 16:21:21 +0200

    Modified engine settings to handle GeoDjango

--- GeoDjangoTuto/settings.py
+++ GeoDjangoTuto/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'GeoDjangoTuto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.mysql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': "geo",
     }
